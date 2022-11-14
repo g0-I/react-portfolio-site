9de1fe06ece5653d203d8437a7d1c1e22fb18486
0k1ta <freeer55@gmail.com>
2022-11-15 01:03:57 +0900

    refactoring/useSkills.js

--- src/customHooks/useSkills.js
+++ src/customHooks/useSkills.js
@@ -1,23 +1,33 @@
 import { useEffect, useReducer } from 'react';
 import axios from 'axios';
+import { requestStates } from '../constants';
 
 import { skillReducer, initialState, actionTypes } from '../reducers/skillReducer';
 
 export const useSkills = () => {
   const [state, dispatch] = useReducer(skillReducer, initialState);
 
+
+const fetchReposApi = () => {
+  axios.get('https://api.github.com/users/g0-I/repos')
+    .then((response) => {
+      const languageList = response.data.map(res => res.language)
+      const countedLanguageList = generateLanguageCountObj(languageList)
+      dispatch({ type: actionTypes.success, payload: {languageList: countedLanguageList } });
+    })
+    .catch(() => {
+      dispatch({ type: actionTypes.error });
+    });
+}
+
+useEffect(() => {
+  if (state.requestStates !== requestStates.loading) { return; }
+  fetchReposApi();
+}, [state.requestStates]);
+
   useEffect(() => {
     dispatch({ type: actionTypes.fetch });
-    axios.get('https://api.github.com/users/g0-I/repos')
-      .then((response) => {
-        const languageList = response.data.map(res => res.language)
-        const countedLanguageList = generateLanguageCountObj(languageList)
-        dispatch({ type: actionTypes.success, payload: { languageList: countedLanguageList } });
-      })
-      .catch(() => {
-        dispatch({ type: actionTypes.error });
-      });
-    }, []);
+  }, []);
 
   const generateLanguageCountObj = (allLanguageList) => {
     const notNullLanguageList = allLanguageList.filter(language => language != null);
@@ -31,9 +41,12 @@ export const useSkills = () => {
     });
   };
 
-  const converseCountToPercentage = (count) => {
-    if (count > 10) { return 100; }
-    return count * 10;
+  const converseCountToPercentage = (languageCount) => {
+    const DEFAULT_MAX_PERCENTAGE = 100;
+    const LANGUAGE_COUNT_BASE = 10;
+
+    if (languageCount > LANGUAGE_COUNT_BASE) { return DEFAULT_MAX_PERCENTAGE; }
+    return languageCount * LANGUAGE_COUNT_BASE;
   };
 
   const sortedLanguageList = () => (
